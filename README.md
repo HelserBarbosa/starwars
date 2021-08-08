5462a004f2464f886cb29f6c1f07262a02dc0ec5
Helder Barbosa <helderlb@gmail.com>
2021-08-08 00:03:46 -0300

    swagger e readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# starwars
+# Projeto StarWars
+
+## Requisitos
+    - Docker
+
+## Como usar
+após clonar o projeto, entrar na pasta raiz do projeto e usar o comando "docker build -t starwars ." para criar a imagem do projeto, após isso usar o comando "docker run -p8080:8080 starwars" para rodar a imagem.
+
+## Swagger
+
+swagger url para auxiliar no uso da api, "http://localhost:8080/star-wars/api/v1/swagger-ui.html"
--- src/main/java/com/b2w/starwars/controller/PlanetController.java
+++ src/main/java/com/b2w/starwars/controller/PlanetController.java
@@ -16,29 +16,52 @@ import com.b2w.starwars.domain.PlanetDomain;
 import com.b2w.starwars.integration.model.PlanetResponse;
 import com.b2w.starwars.services.PlanetService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @Controller
 @RequestMapping("planets")
+@Tag(name = "Planets", description = "api de planetas")
 public class PlanetController {
 
 	@Autowired
 	private PlanetService service;
 
+	@Operation(summary = "Consulta de planetas pela api informada no teste")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "successful", content = @Content(array = @ArraySchema(schema = @Schema(implementation = PlanetResponse.class)))) })
 	@GetMapping
 	public ResponseEntity<PlanetResponse> getPlanets(@RequestParam(value = "page", required = false) Long page,
 			@RequestParam(value = "name", required = false) String name) {
 		return ResponseEntity.ok(this.service.getStarWarsPlanets(page, name));
 	}
 
+	@Operation(summary = "Criação de planetas")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "successful", content = @Content(array = @ArraySchema(schema = @Schema(implementation = PlanetDomain.class)))) })
 	@PostMapping
-	public ResponseEntity<PlanetDomain> postPlanet(@RequestBody PlanetDomain planet) {
+	public ResponseEntity<PlanetDomain> postPlanet(
+			@Parameter(required = true, schema = @Schema(implementation = PlanetDomain.class)) @RequestBody PlanetDomain planet) {
 		return new ResponseEntity<PlanetDomain>(this.service.postPlanet(planet), HttpStatus.CREATED);
 	}
 
+	@Operation(summary = "Consulta de planetas pelo id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "successful", content = @Content(array = @ArraySchema(schema = @Schema(implementation = PlanetDomain.class)))) })
 	@GetMapping("{id}")
 	public ResponseEntity<PlanetDomain> getPlanets(@PathVariable("id") String id) {
 		return ResponseEntity.ok(this.service.getPlanet(id));
 	}
 
+	@Operation(summary = "Listagem de planetas pelo nome")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "successful", content = @Content(array = @ArraySchema(schema = @Schema(implementation = PlanetDomain.class)))) })
 	@GetMapping("list")
 	public ResponseEntity<Page<PlanetDomain>> getPlanets(@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "size", defaultValue = "10") Integer size,
